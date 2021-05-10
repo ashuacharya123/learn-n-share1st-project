bc7ee02c1fc34e1c41dce179fab1c87c5baa528c
ashuacharya123 <71539355+ashuacharya123@users.noreply.github.com>
2021-05-10 13:08:28 +0545

    learn and share

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://ashish:badhuaashish@cluster0.vy24g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+
+  secretOrKey: "secret",
+};
---
+++ server.js
@@ -0,0 +1,39 @@
+//import express from "express";
+const express = require("express");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+const app = express();
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//DB config
+const db = require("./config/keys").mongoURI;
+
+// Connect to mongodb
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+//Passsport middleware
+app.use(passport.initialize());
+
+//Passport Config
+require("./config/passport")(passport);
+
+// use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
